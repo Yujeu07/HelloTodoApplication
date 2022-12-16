01fada3d166a733b6def36896ba927c4459aeebd
phant <phantom4060@gmail.com>
2022-12-16 10:56:19 +0900

    commit

---
+++ TodoApplication/TodoApplication/src/main/java/Hello/TodoApplication/Controller.java
@@ -0,0 +1,12 @@
+package Hello.TodoApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String HelloWorld() {
+        return "To-do Application !";
+    }
+}
